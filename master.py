39446fd85e108e59a2ac1f4a3c605ff48c5a7e72
李静怡 <Emma@lijingyis-MBP.fios-router.home>
2019-04-14 22:56:40 -0400

    corrected name for class dataset

--- master.py
+++ master.py
@@ -3,7 +3,7 @@ import numpy as np
 import csv
 import dataset
 
-class master:
+class master(dataset):
 	def __init__(self, startyear, endyear, month=0, file):
 		dataset.__init__(self, startyear, endyear, month)
 		self.file = file
