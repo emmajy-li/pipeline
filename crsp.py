008adc23c712e561b145df55ee77a47b249820a6
李静怡 <Emma@lijingyis-MBP.fios-router.home>
2019-04-14 22:53:21 -0400

    restructured and added new features for merging

--- crsp.py
+++ crsp.py
@@ -1,83 +1,13 @@
 import pandas as pd
 import numpy as np
 import csv
+import dataset
 
-class crsp:
-	def __init__(self, startyear, endyear, month=0):
-		self.start=startyear
-		self.end=endyear
-		self.month=month
-		self.d={}
-		self.initDataFrame()
+class crsp(dataset):
+	def __init__(self, startyear, endyear, month=0, file):
+		dataset.__init__(self, startyear, endyear, month)
+		self.file = file
 
-	def initDataFrame(self):
-		for y in range(self.start, self.end+1):
-			if self.month==0:
-				for m in range(1,13):
-					filename=self.getfilename(y, m)
-					self.d[filename]=pd.DataFrame()
-			else:
-				filename=self.getfilename(y, self.month)
-				self.d[filename]=pd.DataFrame()
-
-	def getfilename(self, y, m):
-		if m < 10:
-			filename='crsp_{y}0{m}'.format(y=y,m=m)
-		else:
-			filename='crsp_{y}{m}'.format(y=y,m=m)
-		return(filename)
-
-	
-	def split(self, data):
-		dt = data.applymap(str)
-		for y in range(self.start, self.end+1):
-			if self.month==0:
-				for m in range(1,13):
-					filename=self.getfilename(y, m)
-					self.d[filename]=self.d[filename].append(dt.loc[dt['date'].str.startswith(filename[len(filename)-6:])])
-			else:
-				filename=self.getfilename(y, self.month)
-				self.d[filename]=self.d[filename].append(dt.loc[dt['date'].str.startswith(filename[len(filename)-6:])])
-
-	def export(self):
-		for y in range(self.start, self.end+1):
-			if self.month==0:
-				for m in range(1,13):
-					filename=self.getfilename(y, m)
-					with open(filename+'.csv', 'a') as csvFile:
-						self.d[filename].to_csv(csvFile, header=True)
-			else:
-				filename=self.getfilename(y, self.month)
-				with open(filename+'.csv', 'a') as csvFile:
-					self.d[filename].to_csv(csvFile, header=True)
-
-	def merge(self, data, how='left', key):
-		dt = data.applymap(str)
-		for y in range(self.start, self.end+1):
-			if self.month==0:
-				for m in range(1,13):
-					filename=self.getfilename(y, m)
-					self.d[filename]=self.d[filename].merge(right=dt, how=how, on=key)
-			else:
-				filename=self.getfilename(y, self.month)
-				self.d[filename]=self.d[filename].merge(right=dt, how=how, on=key)
-
-	def addsp(self):
-		for y in range(self.start, self.end+1):
-			if self.month==0:
-				for m in range(1,13):
-					filename=self.getfilename(y, m)
-					cond = (self.d[filename]['date']>=self.d[filename]['start']) & (self.d[filename]['date']<=self.d[filename]['ending'])
-					self.d[filename]['sp'] = np.where(cond, 1, 0)
-			else:
-				filename=self.getfilename(y, self.month)
-				cond = (self.d[filename]['date']>=self.d[filename]['start']) & (self.d[filename]['date']<=self.d[filename]['ending'])
-				self.d[filename]['sp'] = np.where(cond, 1, 0)
-
-	def print(self, year, month):
-		filename = self.getfilename(year, month)
-		print("shape: ",self.d[filename].shape)
-		print("head: ",self.d[filename].head())
 
 
 
--- main.py
+++ main.py
@@ -4,29 +4,40 @@ import crsp
 # input
 beginyear = 2008
 endyear = 2018
-month = 2 # if all month then 0
+month = 0 # if all month then 0
+master_prefix = 'master' # the prefix of master filename
 datapath_crsp = 'crsp_10yr.csv'
 datapath_sp = 'sp500list_201502.csv'
 
 # initialization
-crsp1 = crsp.crsp(beginyear, endyear, month)
+c = crsp.crsp(beginyear, endyear, month, 'crsp')
 
 # read in and split
 # total # of rows: 17614314
 for n in range(0,1762):
-	if n==0:
-		data = pd.read_csv(datapath, nrows=10000)
-		crsp1.split(data)
-	else:
-		data = pd.read_csv(datapath, skiprows=range(1,n*10000), nrows=10000)
-		crsp1.split(data)
+		data = pd.read_csv(datapath_crsp, skiprows=range(1,n*10000), nrows=10000)
+		c.split(data)
 
-# merge with splist and add sp indicator
-crsp1.merge(pd.read_csv(datapath_sp), 'left', 'PERMNO')
-crsp1.addsp()
+# merge with splist and 
+c.allmerge(pd.read_csv(datapath_sp), 'left', 'PERMNO')
+
+# add sp indicator
+c.addsp()
+
+# merge with master on eight digit cusip
+for y in range(self.start, self.end+1):
+	if self.month==0:
+		for m in range(1,13):
+			master = pd.read_csv('_'.join([master_prefix, '{y}0{m}']).format(y=y,m=m))
+			master['CUSIP_'] = [value[:8] for value in master['CUSIP']]
+			c.onemerge(master, 'outer', left_on=['CUSIP','date'], right_on=['CUSIP_', 'DATE'], y, m)
+	else:
+		master = pd.read_csv('_'.join(['master', '{y}{m}']).format(y=y,m=m))
+		master['CUSIP_'] = [value[:8] for value in master['CUSIP']]
+		c.onemerge(master, 'outer', left_on=['CUSIP','date'], right_on=['CUSIP_', 'DATE'], y, m)
 
 # print head and shape of data
-crsp1.print(2008,2)
+c.print(2008,2)
 
 # export csv
-crsp1.export()
+c.export()
